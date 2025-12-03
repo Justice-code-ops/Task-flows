08d3fc611f40a0e4d47f1d19b39a3ac8807372a0
Justice-code-ops <nwazuokejustice57@gmail.com>
2025-12-03 09:50:21 +0100

    1. Added a "Dashboard" link to the navbar. 2. Changed "Create account" button to "Create free account", 3. Added "todo.html"

--- server/contentType.js
+++ server/contentType.js
@@ -1,13 +1,11 @@
 export function getContentType(extname) {
     switch (extname) {
-        case '.html':
-            return 'text/html';
-        case '.js':
-            return 'text/javascript';
-        case '.css':
-            return 'text/css';
-        default:
-            return 'application/json';
+        case '.html': return 'text/html';
+        case '.js': return 'text/javascript';
+        case '.css': return 'text/css';
+        case '.png': return 'image/png';
+        case '.jpg': return 'image/jpeg';
+        case '.svg': return 'image/svg+xml';
+        default: return 'application/octet-stream';
     }
 }
-
--- server/server.js
+++ server/server.js
@@ -11,14 +11,14 @@ const PORT = 5000;
 
 const server = http.createServer((req, res) => {
     
-    let filePath = path.join(__dirname, '..', 'public', req.url === '/' ? '/index.html' : req.url);
+    let filePath = path.join(__dirname, '..', 'public', req.url === '/' ? 'index.html' : req.url);
     let extname = path.extname(filePath)
     let contentType = getContentType(extname)
     
     fs.readFile(filePath, (err, content) => {
         if (err) {
             res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end(`Server Error: ${err}}`)
+            res.end(`Server Error: ${err}`)
         } else {
             res.writeHead(200, { 'Content-Type': contentType });
             res.end(content);
