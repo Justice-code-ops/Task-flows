f38863bfd258ebd6fea869dff08c232b5088da87
Justice-code-ops <nwazuokejustice57@gmail.com>
2025-12-04 16:47:13 +0100

    Rebuilt todo page and added the script

---
+++ Public/todo.js
@@ -0,0 +1,40 @@
+const countEl = document.getElementById('task-count')
+const searchEl = document.getElementById('search-input')
+const inputEl = document.getElementById('task-input')
+const emptyEL = document.getElementById('empty-state')
+const btnEl = document.getElementById('add-btn')
+const listEl = document.getElementById('task-list')
+const dateEl = document.getElementById('date')
+const clearEL = document.getElementById('clear-btn')
+
+const date = dateEl.textContent = new Date().getFullYear()
+
+btnEl.addEventListener('click', function() {
+    const task = inputEl.value.trim()
+    let count = null
+
+    if (!task) {
+        return alert('Please add a task')
+    } else {
+        const li = document.createElement('li')
+        const span = document.createElement('span')
+        const input = document.createElement('input')
+        const button = document.createElement('button')
+
+        //set up the elements
+        span.textContent = task;
+        span.classList.add('task-text')
+        button.textContent = 'Delete'
+        button.classList.add('delete-btn')
+
+        //Append Elements to list
+        li.appendChild(span)
+        li.appendChild(button)
+
+        //Append li to listEl
+        listEl.appendChild(li)
+
+        //update task count
+        countEl.textContent = count++
+    }
+})
